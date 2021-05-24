bafbec2f22383c2303571e89a7b54e3ddec84d2d
erasappagari <erasappagari@gmail.com>
2021-05-24 22:25:04 +0530

    Reverted Revert "ipdated1"
    This reverts commit 2ba6635d50f0f61e23d05c76303d4ccb310aa65a.

--- DButils.java
+++ DButils.java
@@ -1,9 +1,6 @@
 this is DButils project
 this DB details
 this is db scripts
-<<<<<<< HEAD
 
 we are adding the code and appending the code
 we r at night time working upto 11pm
-=======
->>>>>>> parent of 6d0393a (updated the code)
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
- revorted 
- this is deploy project
+this is deploy project
 this is deploy scripts
 this is deploy shells
 updated the code in display file
